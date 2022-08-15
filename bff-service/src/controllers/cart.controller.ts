eb51b673907cb3c606331de92281a14079d8274d
Kazik Kluz <kazik.kluz@gmail.com>
2022-08-15 18:21:30 +0100

    Update cart.controller.ts

--- bff-service/src/controllers/cart.controller.ts
+++ bff-service/src/controllers/cart.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Header, Put } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
-import { Observable } from 'rxjs';
 import { Cart, CartItem } from 'src/interfaces/cart.interface';
 import { CartService } from 'src/services/cart.service';
 
@@ -9,13 +8,13 @@ export class CartController {
   constructor(private cartService: CartService) {}
   @Get()
   @Header('Access-Controll-Allow-Origin', '*')
-  findOne(): Observable<AxiosResponse<Cart>> {
+  findOne(): Promise<AxiosResponse<Cart>> {
     return this.cartService.findOne();
   }
 
   @Put()
   @Header('Access-Control-Allow-Origin', '*')
-  put(@Body() cartItems: CartItem[]): Observable<AxiosResponse<Cart>> {
+  put(@Body() cartItems: CartItem[]): Promise<AxiosResponse<Cart>> {
     return this.cartService.put(cartItems);
   }
 }
